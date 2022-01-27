95064e8b059c03188d02ef4f954c49cd59131e9f
Martin Tudor <martcan27@gmail.com>
2022-01-27 19:41:37 +0000

    refactor: shuffle

--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -11,13 +11,15 @@ export class CardContainerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.shuffle()
   }
   shuffle() {
-    const num =  () => Math.floor(Math.random() * 14);
+    const num =  () => Math.floor(Math.random() * 14) // don't pick last index
+    let cardsCopy = this.cards.slice()
     for (let i = 1; i < 32; i++) {
-      const pick = this.cards.splice(num(), 1)[0]
-      this.cards = [...this.cards, pick]
+      const pick = cardsCopy.splice(num(), 1)[0]
+      cardsCopy = [...cardsCopy, pick]
     }
+    this.cards = cardsCopy
   }
 }
