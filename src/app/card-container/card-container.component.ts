367a9a732f17784eefb6c68970c3f7221d164351
Martin Tudor <martcan27@gmail.com>
2022-01-27 20:14:52 +0000

    check cards

--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-container.component.css']
 })
 export class CardContainerComponent implements OnInit {
-  cards: any = ["dog", "cat", "horse", "mouse", "butterfly", "bird", "snake", "pig", "dog", "cat", "horse", "mouse", "butterfly", "bird", "snake", "pig"]
-  
+  cards = ["dog", "cat", "horse", "mouse", "butterfly", "bird", "snake", "pig", "dog", "cat", "horse", "mouse", "butterfly", "bird", "snake", "pig"]
+  matched: string[] = []
+  check: number[] = []
   constructor() { }
 
   ngOnInit(): void {
@@ -21,5 +22,19 @@ export class CardContainerComponent implements OnInit {
       cardsCopy = [...cardsCopy, pick]
     }
     this.cards = cardsCopy
+  }
+  checkCards() {
+    if(this.cards[this.check[0]] === this.cards[this.check[1]]) {
+      this.matched = [...this.matched, this.cards[this.check[0]]]
+    } else {
+      this.check = [] // close cards
+    }
+    console.log(this.matched)
+  }
+  openCard (index: number) {
+    if(this.check.length == 2) return
+    this.check.push(index)
+    console.log(this.check)
+    if(this.check.length ==2 ) return this.checkCards()
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   @Input() card!: string
+  @Input() i!: number
   constructor() { }
 
   ngOnInit(): void {
