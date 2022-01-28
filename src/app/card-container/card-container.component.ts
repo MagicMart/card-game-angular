9694e7b80bccef234f775caa43334abe4993919f
MagicMart <martcan27@gmail.com>
2022-01-28 10:28:03 +0000

    feature: show opened and closed

--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -3,38 +3,61 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-container',
   templateUrl: './card-container.component.html',
-  styleUrls: ['./card-container.component.css']
+  styleUrls: ['./card-container.component.css'],
 })
 export class CardContainerComponent implements OnInit {
-  cards = ["dog", "cat", "horse", "mouse", "butterfly", "bird", "snake", "pig", "dog", "cat", "horse", "mouse", "butterfly", "bird", "snake", "pig"]
-  matched: string[] = []
-  check: number[] = []
-  constructor() { }
+  cards = [
+    'dog',
+    'cat',
+    'horse',
+    'mouse',
+    'butterfly',
+    'bird',
+    'snake',
+    'pig',
+    'dog',
+    'cat',
+    'horse',
+    'mouse',
+    'butterfly',
+    'bird',
+    'snake',
+    'pig',
+  ];
+  matched: string[] = [];
+  opened: number[] = [];
+  constructor() {}
 
   ngOnInit(): void {
-    this.shuffle()
+    this.shuffle();
   }
   shuffle() {
-    const num =  () => Math.floor(Math.random() * 14) // don't pick last index
-    let cardsCopy = this.cards.slice()
+    const num = () => Math.floor(Math.random() * 14); // don't pick last index
+    let cardsCopy = this.cards.slice();
     for (let i = 1; i < 32; i++) {
-      const pick = cardsCopy.splice(num(), 1)[0]
-      cardsCopy = [...cardsCopy, pick]
+      const pick = cardsCopy.splice(num(), 1)[0];
+      cardsCopy = [...cardsCopy, pick];
     }
-    this.cards = cardsCopy
+    this.cards = cardsCopy;
+  }
+
+  closeCards() {
+    setTimeout(() => (this.opened = []), 900);
   }
   checkCards() {
-    if(this.cards[this.check[0]] === this.cards[this.check[1]]) {
-      this.matched = [...this.matched, this.cards[this.check[0]]]
+    if (this.cards[this.opened[0]] === this.cards[this.opened[1]]) {
+      this.matched = [...this.matched, this.cards[this.opened[0]]];
+      setTimeout(() => (this.opened = []), 600);
     } else {
-      this.check = [] // close cards
+      this.closeCards(); // close cards
     }
-    console.log(this.matched)
+    console.log(this.matched);
   }
-  openCard (index: number) {
-    if(this.check.length == 2) return
-    this.check.push(index)
-    console.log(this.check)
-    if(this.check.length ==2 ) return this.checkCards()
+  openCard(index: number) {
+    if (this.opened.length == 2) return;
+    if (this.opened.includes(index)) return;
+    this.opened.push(index);
+    console.log(this.opened);
+    if (this.opened.length == 2) return this.checkCards();
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-  @Input() card!: string
-  @Input() i!: number
-  constructor() { }
+  @Input() card!: string;
+  @Input() i!: number;
+  @Input() opened!: boolean;
+  @Input() matched!: boolean;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
