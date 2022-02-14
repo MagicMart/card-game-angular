77edf50d9623fedffb0067d52bc83a1b5c054980
MagicMart <martcan27@gmail.com>
2022-02-14 20:14:25 +0000

    use title service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'card-game';
+  title = 'Card Game';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardContainerComponent } from './card-container/card-container.component';
@@ -14,7 +14,7 @@ import { ZeroPadPipe } from '../zero-pad.pipe';
     ZeroPadPipe,
   ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [Titl],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
