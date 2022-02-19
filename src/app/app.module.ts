3f8cafbce3ca673e8371d08bfce5f01e4696062a
MagicMart <martcan27@gmail.com>
2022-02-19 19:59:15 +0000

    rmove unnecessary custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,9 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardContainerComponent } from './card-container/card-container.component';
 import { CardComponent } from './card/card.component';
-import { ZeroPadPipe } from '../zero-pad.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardContainerComponent,
-    CardComponent,
-    ZeroPadPipe,
-  ],
+  declarations: [AppComponent, CardContainerComponent, CardComponent],
   imports: [BrowserModule],
   providers: [Title],
   bootstrap: [AppComponent],
